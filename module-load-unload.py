5425804c15c769723cf60acf8ab3ff63a2625d83
Johannes Thumshirn <jthumshirn@suse.de>
2016-06-08 16:38:46 +0200

    module-load-unload.py: Get list of loaded modules
    Get list of loaded modules via python-kmod.
    
    Signed-off-by: Johannes Thumshirn <jthumshirn@suse.de>

--- module-load-unload.py
+++ module-load-unload.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
+import kmod
 from modlist import SystemModules
 
 def main():
+    km = kmod.Kmod()
+    modules_loaded = [str(m.name) for m in km.loaded()]
     modules = SystemModules.get_installed_modules()
     print(len(modules))
 
