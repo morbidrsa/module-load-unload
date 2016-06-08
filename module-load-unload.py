99cea5790c213eb55e9de4c01d70e077bb733e6b
Johannes Thumshirn <jthumshirn@suse.de>
2016-06-08 16:46:30 +0200

    module-load-unload.py: Print header
    Print small header with the amount of modules installed and the amount of
    modules loaded as well as the delta used in the load unload tests.
    
    Signed-off-by: Johannes Thumshirn <jthumshirn@suse.de>

--- module-load-unload.py
+++ module-load-unload.py
@@ -6,7 +6,14 @@ def main():
     km = kmod.Kmod()
     modules_loaded = [str(m.name) for m in km.loaded()]
     modules_installed = SystemModules.get_installed_modules()
-    print(len(modules_installed))
+    num_installed = len(modules_installed)
+    num_loaded = len(modules_loaded)
+    delta = num_installed - num_loaded
+
+    print("%d Kernel modules installed, %d loaded" % (num_installed,
+        num_loaded))
+    print("Performing random load/unload test on %d kernel modules" % delta)
+
 
 
 if __name__ == '__main__':
