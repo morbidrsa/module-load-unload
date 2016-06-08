4d671dbc1875087fe4471b317327406222c350f7
Johannes Thumshirn <jthumshirn@suse.de>
2016-06-08 16:24:15 +0200

    Factor out ModuleList
    Factor out ModuleList into it's own file.
    
    Signed-off-by: Johannes Thumshirn <jthumshirn@suse.de>

--- module-load-unload.py
+++ module-load-unload.py
@@ -1,27 +1,5 @@
 #!/usr/bin/env python
-import os
-
-class ModuleList:
-    def __init__(self):
-        pass
-
-    @staticmethod
-    def __get_kernel_release():
-        sysname, nodename, release, version, machine = os.uname()
-        return release
-
-    @classmethod
-    def get_installed_modules(cls):
-        modules = []
-        release = cls.__get_kernel_release()
-
-        moddir = "/lib/modules/" + release + "/kernel/"
-        for root, dirs, files in os.walk(moddir):
-            for file in files:
-                if file.endswith('.ko'):
-                    modules.append(file)
-
-        return modules
+from modlist import ModuleList
 
 def main():
     modules = ModuleList.get_installed_modules()
