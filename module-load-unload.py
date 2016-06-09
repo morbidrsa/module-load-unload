3c1035cdaf9f527e86c5041a25f3451abeda202f
Johannes Thumshirn <jthumshirn@suse.de>
2016-06-09 11:52:11 +0200

    module-load-unload.py: Build list of usable modules
    Build a list of usable modules by reducing the list of installed kernel modules
    by the list of loaded modules.
    
    We do this to not accidently remove modules that are needed for the operation
    of the test host.
    
    Signed-off-by: Johannes Thumshirn <jthumshirn@suse.de>

--- module-load-unload.py
+++ module-load-unload.py
@@ -6,13 +6,15 @@ def main():
     km = kmod.Kmod()
     modules_loaded = [str(m.name) for m in km.loaded()]
     modules_installed = SystemModules.get_installed_modules()
+    modules_usable = [x for x in modules_installed if x[:-3].replace('-', '_')
+            not in modules_loaded]
     num_installed = len(modules_installed)
     num_loaded = len(modules_loaded)
-    delta = num_installed - num_loaded
+    num_usable = len(modules_usable)
 
     print("%d Kernel modules installed, %d loaded" % (num_installed,
         num_loaded))
-    print("Performing random load/unload test on %d kernel modules" % delta)
+    print("Performing random load/unload test on %d kernel modules" % num_usable)
 
 
 
